2378ba8e5a73c46af8787cef2da9e4c0cb992149
hlamija <lhajro1@etf.unsa.ba>
2014-03-21 12:17:24 +0100

    Lamija commit

---
+++ FFBHPL/FFBHPL/ITeamService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Newtonsoft.Json;
+namespace FFBHPL
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITeamService" in both code and config file together.
+    [ServiceContract]
+    public interface ITeamService
+    {
+        [OperationContract]
+        JsonObjectAttribute readTeam(int teamId);
+        [OperationContract]
+        JsonObjectAttribute readAllTeams();
+        [OperationContract]
+        bool createTeam(JsonObjectAttribute input);
+        [OperationContract]
+        JsonObjectAttribute updateTeam(JsonObjectAttribute input);
+        [OperationContract]
+        bool deleteTeam(int seasonId);
+    }
+}
